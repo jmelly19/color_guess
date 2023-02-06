12f5d2c8042f5cff12ed04c0b95cbb63a653fb2c
Jared Meldrum <90654310+jmelly19@users.noreply.github.com>
2023-02-06 14:11:56 -0700

    Deletecolor.ps1

--- color.ps1
+++
@@ -1,8 +0,0 @@
-$yours=Read-host "What is your favorite color?"
-$mine='blue'
-if($yours -eq $mine){
-    write-host "Our favorite colors are the same!"
-}
-else{
-    "We don't have the same favorite color."
-}
